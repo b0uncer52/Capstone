0b4c041d692fffec621822008184709494e32dd1
Jason Smith <jasonlsmith52@gmail.com>
2017-07-05 22:19:35 -0500

    Delete FBCreds.js

--- app/values/FBCreds.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-app.constant('FBCreds', {
-    apiKey: "AIzaSyCMJIDyf7nEhIIDU-bomr0eoSFir8k4NUA",
-    authDomain: "gametime-16533.firebaseapp.com",
-    databaseURL: "https://gametime-16533.firebaseio.com"
-});
