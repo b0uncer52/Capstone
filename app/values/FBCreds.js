449c396a8100df4c1713c28446d40b87ab865ca2
Jason Smith <jasonlsmith52@gmail.com>
2017-07-20 14:37:23 -0500

    added values

---
+++ app/values/FBCreds.js
@@ -0,0 +1,7 @@
+'use strict';
+
+app.constant('FBCreds', {
+    apiKey: "AIzaSyCMJIDyf7nEhIIDU-bomr0eoSFir8k4NUA",
+    authDomain: "gametime-16533.firebaseapp.com",
+    databaseURL: "https://gametime-16533.firebaseio.com"
+});
